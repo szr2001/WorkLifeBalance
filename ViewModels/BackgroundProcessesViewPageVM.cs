1d8dd017441ac10c0296e67852f2a0bc0cab01c1
Szabo Robert <szr2001@yahoo.com>
2024-09-03 20:11:24 +0300

    Moved BackgroundProcessesView logic in Vm

---
+++ ViewModels/BackgroundProcessesViewPageVM.cs
@@ -0,0 +1,87 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Numerics;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+using WorkLifeBalance.Services;
+using WorkLifeBalance.Services.Feature;
+using CommunityToolkit.Mvvm.Input;
+
+namespace WorkLifeBalance.ViewModels
+{
+    public partial class BackgroundProcessesViewPageVM : SecondWindowPageVMBase
+    {
+        public ObservableCollection<string> DetectedWindows { get; set; } = new();
+        public ObservableCollection<string> SelectedWindows { get; set; } = new();
+
+        [ObservableProperty]
+        private string activeWindow = "";
+
+        private DataStorageFeature dataStorageFeature;
+        private LowLevelHandler lowLevelHandler;
+        private ActivityTrackerFeature activityTrackerFeature;
+        public BackgroundProcessesViewPageVM(DataStorageFeature dataStorageFeature, LowLevelHandler lowLevelHandler, ActivityTrackerFeature activityTrackerFeature)
+        {
+            RequiredWindowSize = new Vector2(700, 570);
+            WindowPageName = "Customize Work Apps";
+            this.dataStorageFeature = dataStorageFeature;
+            this.lowLevelHandler = lowLevelHandler;
+            this.activityTrackerFeature = activityTrackerFeature;
+            activityTrackerFeature.OnWindowChange += UpdateActiveWindowUi;
+            InitializeProcessNames();
+        }
+
+        private void UpdateActiveWindowUi(string newwindow)
+        {
+            ActiveWindow = newwindow;
+        }
+
+        private void InitializeProcessNames()
+        {
+            SelectedWindows = new ObservableCollection<string>(dataStorageFeature.AutoChangeData.WorkingStateWindows);
+            DetectedWindows = new ObservableCollection<string>(lowLevelHandler.GetBackgroundApplicationsName());
+            DetectedWindows = new ObservableCollection<string>(DetectedWindows.Except(SelectedWindows));
+        }
+
+        public override async Task ClosePageAsync()
+        {
+            activityTrackerFeature.OnWindowChange -= UpdateActiveWindowUi;
+            dataStorageFeature.AutoChangeData.WorkingStateWindows = SelectedWindows.ToArray();
+            await dataStorageFeature.SaveData();
+        }
+
+        [RelayCommand]
+        private void ReturnToPreviousPage()
+        {
+            SecondWindow.RequestSecondWindow(SecondWindowType.Settings);
+        }
+
+        [RelayCommand]
+        private void SelectProcess(object sender)
+        {
+            Button button = (Button)sender;
+            TextBlock textBlock = (TextBlock)button.Content;
+
+            if (DetectedWindows.Contains(textBlock.Text))
+            {
+                DetectedWindows.Remove(textBlock.Text);
+                SelectedWindows.Add(textBlock.Text);
+            }
+        }
+
+        [RelayCommand]
+        private void DeselectProcess(object sender)
+        {
+            Button button = (Button)sender;
+            TextBlock textBlock = (TextBlock)button.Content;
+
+            if (SelectedWindows.Contains(textBlock.Text))
+            {
+                SelectedWindows.Remove(textBlock.Text);
+                DetectedWindows.Add(textBlock.Text);
+            }
+        }
+    }
+}
---
+++ Views/BackgroundProcessesViewPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using WorkLifeBalance.ViewModels;
+
+namespace WorkLifeBalance.Views
+{
+    /// <summary>
+    /// Interaction logic for BackgroundWindowsViewPage.xaml
+    /// </summary>
+    public partial class BackgroundProcessesViewPage : Page
+    {
+        private BackgroundProcessesViewPageVM backgroundWindowsViewPageVM;
+        public BackgroundProcessesViewPage(BackgroundProcessesViewPageVM backgroundWindowsViewPageVM)
+        {
+            InitializeComponent();
+            this.backgroundWindowsViewPageVM = backgroundWindowsViewPageVM;
+            DataContext = backgroundWindowsViewPageVM;
+        }
+    }
+}
