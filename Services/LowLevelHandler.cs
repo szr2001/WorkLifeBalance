bd9f43dfe81eb1322eff4f4118a26a3aa1191061
Szabo Robert <szr2001@yahoo.com>
2024-10-28 17:14:28 +0200

    Cleared LowLevelhandler

--- Services/LowLevelHandler.cs
+++ Services/LowLevelHandler.cs
@@ -13,10 +13,6 @@ namespace WorkLifeBalance.Services
     //low level handling of windows and mouse
     public class LowLevelHandler
     {
-        // Constants for the SetWindowLoc functions
-        public const uint SWP_NOSIZE = 0x0001;
-        public const uint SWP_NOZORDER = 0x0004;
-        //read mouse pos
         [DllImport("user32.dll")]
         private static extern bool GetCursorPos(out POINT lpPoint);
 
@@ -32,7 +28,6 @@ namespace WorkLifeBalance.Services
         [DllImport("user32.dll")]
         [return: MarshalAs(UnmanagedType.Bool)]
         private static extern bool EnumWindows(EnumWindowsProc lpEnumFunc, nint lParam);
-        // Delegate for EnumWindows for callback
         private delegate bool EnumWindowsProc(nint hWnd, nint lParam);
 
         [DllImport("user32.dll")]
@@ -40,22 +35,10 @@ namespace WorkLifeBalance.Services
         public static extern bool IsWindowVisible(nint hWnd);
 
         [DllImport("user32.dll")]
-        private static extern nint FindWindow(string? lpClassName, string lpWindowName);
-
-        [DllImport("user32.dll")]
-        private static extern bool SetWindowPos(nint hWnd, nint hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
-
-        [DllImport("user32.dll")]
         private static extern nint GetForegroundWindow();
-
-
-        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
-        private static extern int GetWindowText(nint hWnd, StringBuilder lpString, int nMaxCount);
-
 
         [DllImport("user32.dll", SetLastError = true)]
         private static extern uint GetWindowThreadProcessId(nint hWnd, out uint lpdwProcessId);
-
 
         [DllImport("psapi.dll")]
         private static extern uint GetModuleFileNameEx(nint hProcess, nint hModule, StringBuilder lpBaseName, int nSize);
@@ -131,25 +114,6 @@ namespace WorkLifeBalance.Services
         public nint ReadForegroundWindow()
         {
             return GetForegroundWindow();
-        }
-
-        public void SetWindowLocation(nint windowHandle, int x, int y)
-        {
-            // Call SetWindowPos with the SWP_NOSIZE and SWP_NOZORDER flags
-            SetWindowPos(windowHandle, nint.Zero, x, y, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
-        }
-
-        public nint GetWindow(string? lpClassName, string lpWindowName)
-        {
-            return FindWindow(lpClassName, lpWindowName);
-        }
-
-        public string GetWindowTitle(nint hWnd)
-        {
-            const int nChars = 256;
-            StringBuilder windowTitle = new StringBuilder(nChars);
-            GetWindowText(hWnd, windowTitle, nChars);
-            return windowTitle.ToString();
         }
 
         public string GetProcessname(nint hWnd)
