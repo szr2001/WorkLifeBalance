337a3ef1488765f238d1388ca644b5cecc1f61b1
Szabo Robert <szr2001@yahoo.com>
2024-12-06 14:52:07 +0200

    Force state fully working

--- ViewModels/ForceStateMainMenuDetailsPageVM.cs
+++ ViewModels/ForceStateMainMenuDetailsPageVM.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Threading.Tasks;
 using WorkLifeBalance.Interfaces;
 using WorkLifeBalance.Services.Feature;
 using WorkLifeBalance.ViewModels.Base;
@@ -7,12 +10,36 @@ namespace WorkLifeBalance.ViewModels
 {
     public partial class ForceStateMainMenuDetailsPageVM : MainWindowDetailsPageBase
     {
+        [ObservableProperty]
+        private AppState forcedAppState = AppState.Resting;
+
         private readonly IFeaturesServices featuresServices;
         private readonly ForceStateFeature forceStateFeature;
+
+        private int AppstatesCount;
         public ForceStateMainMenuDetailsPageVM(ForceStateFeature forceStateFeature, IFeaturesServices featuresServices)
         {
             this.forceStateFeature = forceStateFeature;
             this.featuresServices = featuresServices;
+            AppstatesCount = Enum.GetValues(typeof(AppState)).Length - 1;
+        }
+
+        partial void OnForcedAppStateChanged(AppState value)
+        {
+            forceStateFeature.SetForcedAppState(value);
+        }
+
+        [RelayCommand]
+        private void ChangeForcedState()
+        {
+            if((int)ForcedAppState == AppstatesCount)
+            {
+                ForcedAppState = 0;
+            }
+            else
+            {
+                ForcedAppState++;
+            }
         }
 
         public override Task OnPageClosingAsync()
